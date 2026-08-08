53de527f6651d10fabbcdd4114282de68bde14e5
pritam soni <pritam.beeztech@gmail.com>
2026-08-08 17:22:49 +0530

    Add vite project

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+import Text from "./Text"
+function App() {
+  return (
+    <>
+   <h1>welcome to my web page | pritam </h1>
+ <Text/>
+ </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Text.jsx
@@ -0,0 +1,9 @@
+function  Text()
+{
+    return (
+    <h3>web page is ready </h3>
+    )
+}
+export default Text
+
+
